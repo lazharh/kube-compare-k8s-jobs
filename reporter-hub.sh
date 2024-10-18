0a9e7b636c5525887c7ba9dc903e1ba95e634014
Borball Zhai <borball.zh@gmail.com>
2024-10-18 16:36:52 -0400

    add more debug info

--- reporter-hub.sh
+++ reporter-hub.sh
@@ -8,8 +8,12 @@ if [ $(oc get managedcluster.cluster.open-cluster-management.io   |grep $spoke |
   echo "---------------------- comparing cluster: $spoke with $metadata ----------------------"
   oc get secret -n ${spoke} ${spoke}-admin-kubeconfig -o jsonpath={.data.kubeconfig} |base64 -d > kubeconfig-${spoke}.yaml
   export KUBECONFIG=kubeconfig-${spoke}.yaml
+  echo "cluster info:"
   oc get clusterversion
   echo
+  oc get node
+  echo
+  echo "all policies applied on the cluster sorted by wave:"
   oc get policy -A -o=custom-columns=NS:.metadata.namespace,NAME:.metadata.name,"REMEDIATION ACTION":.spec.remediationAction,"COMPLIANCE STATE":.status.compliant,WAVE:.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave" --sort-by={.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave"}
   echo
   echo "running oc cluster-compare -r /kube-compare-reference/$metadata:"
--- reporter-spoke.sh
+++ reporter-spoke.sh
@@ -3,9 +3,19 @@
 metadata="metadata.yaml"
 
 echo "---------------------- comparing cluster with $metadata ----------------------"
+echo "cluster info:"
 oc get clusterversion
 echo
-oc get policy -A -o=custom-columns=NS:.metadata.namespace,NAME:.metadata.name,"REMEDIATION ACTION":.spec.remediationAction,"COMPLIANCE STATE":.status.compliant,WAVE:.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave" --sort-by={.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave"}
+oc get node
+
+if [ $(oc api-resources --api-group=policy.open-cluster-management.io |wc -l) -eq 1 ]; then
+  #not managed by Red Hat ACM
+  echo
+else
+  echo "all policies applied on the cluster sorted by wave:"
+  oc get policy -A -o=custom-columns=NS:.metadata.namespace,NAME:.metadata.name,"REMEDIATION ACTION":.spec.remediationAction,"COMPLIANCE STATE":.status.compliant,WAVE:.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave" --sort-by={.metadata.annotations."ran\.openshift\.io\/ztp-deploy-wave"}
+fi
+
 echo
 echo "running oc cluster-compare -r /kube-compare-reference/$metadata:"
 oc cluster-compare -r /kube-compare-reference/$metadata
